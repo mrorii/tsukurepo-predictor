9034f0f22255e6f45b10d9d165ec8dff4788d267
Naoki Orii <mrorii@gmail.com>
2013-07-29 21:22:41 -0700

    more preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -5,30 +5,39 @@ import re
 import zenhan
 
 SURROUNDS = (
-    re.compile(ur'\(.*\)'),
-    re.compile(ur'\（.*\）'),
-    re.compile(ur'\[.*\]'),
+    re.compile(ur'\(.*\).*'),
+    re.compile(ur'\(.*\）.*'),
+    re.compile(ur'\（.*\）.*'),
+    re.compile(ur'\（.*\).*'),
+    re.compile(ur'\[.*\].*'),
+    re.compile(ur'\<.*\>.*'),
+    re.compile(ur'＜.*＞.*'),
+    re.compile(ur'【.*】.*'),
 )
 
 UNCLOSED_PAREN = re.compile(ur"(\w+)\([^(]+", re.UNICODE)
 
 STARTS_WITH_ALPHA = re.compile(ur"^[a-zA-Z][.:()（）\s]*([^a-zA-Z]+)")
 
-SPLIT = re.compile(ur'or|OR|または|又は|/|\+|あれば')
+ENDS_WITH = re.compile(ur"(等|など)$")
+
+SPLIT = re.compile(ur'or|OR|または|又は|/|\+|あれば|、|・')
 
 OPTIONAL_START = re.compile(ur'^(好みの|お好みの|お好みにより|あれば|お好きの|お好きな|市販の)')
 
 SPECIAL_SYMBOLS = (
     re.compile(ur'\*'),
-    re.compile(ur'\u30fb'),
-    re.compile(ur'[\u2000-\u206f]'),  # punctuation
-    re.compile(ur'[\u2460-\u24ff]'),  # enclosed alphanumerics
-    re.compile(ur'[\u2500-\u257f]'),  # box drawing
-    re.compile(ur'[\u25a0-\u25ff]'),  # geometric shapes
-    re.compile(ur'[\u2600-\u26ff]'),  # miscellaneous symbols
-    re.compile(ur'[\u2700-\u27bf]'),  # dingbats
-    re.compile(ur'[\u3000-\u303f]'),  # cjk symbols and punctuation
-    re.compile(ur'[\uff00-\uffef]'),  # halfwidth and fullwdith forms
+    re.compile(ur'@'),
+    # re.compile(ur'\u30fb'),                  # "・"
+    re.compile(ur'[\u2000-\u206f]'),         # punctuation
+    re.compile(ur'[\u2460-\u24ff]'),         # enclosed alphanumerics
+    re.compile(ur'[\u2500-\u257f]'),         # box drawing
+    re.compile(ur'[\u25a0-\u25ff]'),         # geometric shapes
+    re.compile(ur'[\u2600-\u26ff]'),         # miscellaneous symbols
+    re.compile(ur'[\u2700-\u27bf]'),         # dingbats
+    re.compile(ur'[\u3000|\u3002-\u303f]'),  # cjk symbols and punctuation
+                                             # except "、"
+    re.compile(ur'[\uff00-\uffef]'),         # halfwidth and fullwdith forms
 )
 
 def make_function_hiragana():
@@ -51,7 +60,8 @@ def normalize(ingredient):
     if match:
         ingredient = match.groups()[0]
 
-    ingredient = zenhan.h2z(ingredient, mode=4)  # only deal with katakana
+    ingredient = zenhan.z2h(ingredient, mode=1)  # ascii
+    ingredient = zenhan.h2z(ingredient, mode=4)  # kana
 
     # convert all katakana to hiragana
     ingredient = hiragana(ingredient)
@@ -63,10 +73,12 @@ def normalize(ingredient):
     for SPECIAL_SYMBOL in SPECIAL_SYMBOLS:
         ingredient = SPECIAL_SYMBOL.sub(lambda s: '', ingredient)
 
-    ingredients = map(lambda ingr: ingr.strip(), SPLIT.split(ingredient))
+    ingredients = SPLIT.split(ingredient)
+    ingredients = map(lambda ingr: ENDS_WITH.sub(lambda s: '', ingr), ingredients)
+    ingredients = map(lambda ingr: ingr.strip(), ingredients)
+    ingredients = filter(lambda ingr: ingr, ingredients)
+
     for ingredient in ingredients:
-        if not ingredient:
-            continue
         yield ingredient
 
 if __name__ == '__main__':
@@ -86,6 +98,16 @@ if __name__ == '__main__':
         u'お好きな葉野菜',
         u'酒あれば泡盛',
         u'市販のカレールー',
+        u'しめじ、えのき等',
+        u'キウリ、ハム、錦糸卵 ',
+        u'水・ごま油・酒・すり胡麻',
+        u'ニンニク(みじん切り)...A ',
+        u'ニンニク<みじん切り>...A ',
+        u'ニンニク＜みじん切り＞...A ',
+        u'豚薄切り肉【巻ける位のものなら何でも】',
+        u'一味唐辛子or七味唐辛子',
+        u'生Cilantro（コリアンダー）',
+        u'@醤油',
     ]
 
     for ingredient in ingredients:
