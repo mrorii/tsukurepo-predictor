30d7a99f5618be51ec64b5d7675466575eda2e43
Naoki Orii <mrorii@gmail.com>
2013-07-31 21:02:20 -0400

    ignore new recipes

--- split_data.py
+++ split_data.py
@@ -5,11 +5,24 @@ import logging
 import argparse
 import random
 import json
+import time
+import datetime
 
 def load_data(filename):
     with open(filename, 'r') as f:
         for line in f:
             yield json.loads(line.strip())
+
+def read_date(date_str):
+    try:
+        return time.strptime(date_str, '%Y-%m-%d')
+    except:
+        return time.strptime(date_str, '%y/%m/%d')
+
+def has_progressed(days, t1, t2):
+    t1 = datetime.datetime.fromtimestamp(time.mktime(t1))
+    t2 = datetime.datetime.fromtimestamp(time.mktime(t2))
+    return (t2 - t1).days > days
 
 def main():
     logging.basicConfig(level=logging.INFO, format='%(message)s')
@@ -19,11 +32,16 @@ def main():
     parser.add_argument('output_dir')
     parser.add_argument('--threshold', type=int, default=10)
     parser.add_argument('--seed', type=int, default=0)
+    parser.add_argument('--crawl_date', type=read_date, default='2013-07-22')
     args = parser.parse_args()
 
     random.seed(args.seed)
 
-    data = list(load_data(args.json))
+    data = load_data(args.json)
+    data = filter(lambda d: has_progressed(90,
+                                           read_date(d['published_date']),
+                                           args.crawl_date), data)
+    data = list(data)  # convert generator into list
     pos_data = filter(lambda d: d['report_count'] >= args.threshold, data)
     neg_data = filter(lambda d: d['report_count'] < args.threshold, data)
 
